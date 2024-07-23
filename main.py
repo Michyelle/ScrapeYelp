b0c022212575141724874fba88781377554fd325
michyelle <michellenguyen12@csu.fullerton.edu>
2024-07-23 10:57:06 -0700

    No repeated companies, and works as expected

--- main.py
+++ main.py
@@ -8,13 +8,24 @@ from webdriver_manager.chrome import ChromeDriverManager
 import csv
 import time
 import re
+import json
+import os
+import pandas as pd
 
 # Open the CSV file in append mode
 with open('vets_in_LA.csv', 'a', newline='') as file:
     writer = csv.writer(file)
     # Write the headers only if the file is empty
     if file.tell() == 0:
-        writer.writerow(["Company Name", "Phone Number", "Address"])
+        writer.writerow(["Company Name", "Phone Number", "Address", "Company URL"])
+
+if os.path.exists('vets_in_LA.csv'):
+    # If the file exists, load the data
+    df = pd.read_csv('vets_in_LA.csv')
+    visited_urls = set(df["Company URL"].tolist())
+else:
+    # If the file doesn't exist, initialize an empty set
+    visited_urls = set()
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--ignore-ssl-errors=yes')
@@ -25,46 +36,44 @@ service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=chrome_options)
 driver.maximize_window()
 
-def extract_company_info(driver, writer):
+def get_company_info(driver, writer):
+    # Get the company's name
+    company_name_element = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "y-css-olzveb"))
+    )
+    company_name = company_name_element.text
+
+    # Get the phone number
+    phone_number = "Number not found" 
     try:
-        # Get the company's name
-        company_name_element = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "y-css-olzveb"))
+        phone_number_element = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'y-css-1lfp6nf')]//p[contains(@class, 'y-css-1o34y7f') and contains(text(), '(')]"))
         )
-        company_name = company_name_element.text
+        phone_number_text = phone_number_element.text
+        # Looks for the (xxx) xxx-xxxx
+        phone_number_match = re.search(r'\(\d{3}\) \d{3}-\d{4}', phone_number_text)
+        # Returns exactly (xxx) xxx-xxxx
+        phone_number = phone_number_match.group(0)
+    except:
+        print("Company number not found")
 
-        # Get the phone number
-        try:
-            phone_number_element = WebDriverWait(driver, 5).until(
-                EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'y-css-1lfp6nf')]//p[contains(@class, 'y-css-1o34y7f') and contains(text(), '(')]")
-            ))
-            phone_number_text = phone_number_element.text
-            phone_number_match = re.search(r'\(\d{3}\) \d{3}-\d{4}', phone_number_text)
-            phone_number = phone_number_match.group(0) if phone_number_match else "Not found"
-        except:
-            print("Company number not found")
-            phone_number = "num Not found"
+    # Get the address
+    address = "Address not found"
+    try:
+        address_elements = WebDriverWait(driver, 5).until(
+            EC.presence_of_all_elements_located((By.CLASS_NAME, "y-css-dg8xxd"))
+        )
+        # There are different texts with the same class for address, so search for the ones that start with at least 3 digits
+        for element in address_elements:
+            if re.search(r'\d{3,}.*', element.text):
+                address = element.text
+                break
+    except:
+        print("Address not found")
 
-        # Get the address
-        try:
-            address_elements = WebDriverWait(driver, 5).until(
-                EC.presence_of_all_elements_located((By.CLASS_NAME, "y-css-dg8xxd"))
-            )
-            address = "address not found"
-            for element in address_elements:
-                if re.search(r'\d{3,}.*', element.text):
-                    address = element.text.strip()
-                    break
-        except:
-            print("Address not found")
-            address = "address not found"
-
-        # Write data to CSV
-        writer.writerow([company_name, phone_number, address])
-        print(f"Company name, number, and address written to vets_in_LA.csv: {company_name}, {phone_number}, {address}")
-
-    except Exception as e:
-        print(f"Error extracting company info: {e}")
+    # Write data to CSV
+    writer.writerow([company_name, phone_number, address, company_url])
+    print(f"Company name, number, and address written to vets_in_LA.csv: {company_name}, {phone_number}, {address} {company_url}")
 
 try:
     driver.get("https://www.yelp.com")
@@ -73,19 +82,17 @@ try:
     location_field = driver.find_element(By.ID, "search_location")
 
     search_field.clear()
-    search_field.send_keys("veterinaries")
+    search_field.send_keys("vets")
     location_field.send_keys(Keys.CONTROL + "a")
     location_field.send_keys(Keys.DELETE)
     location_field.send_keys("Los Angeles, CA")
-
     location_field.send_keys(Keys.RETURN)
-    time.sleep(5)  # Adjust wait time as needed
-
-    visited_urls = set()
     page_number = 1
 
     while True:
-        for i in range(13, 24):  # last 2 results in the page
+        error_count = 0
+        # Grabs every result that isnt sponsored (1-10 each page)
+        for i in range(13, 24):
             try:
                 # Find the company link
                 company_link = WebDriverWait(driver, 5).until(
@@ -94,23 +101,25 @@ try:
                 company_url = company_link.get_attribute('href')
 
                 if company_url in visited_urls:
+                    print("Already seen")
                     continue
 
                 visited_urls.add(company_url)
                 driver.get(company_url)
-                time.sleep(3)  # Adjust wait time as needed
 
                 with open('vets_in_LA.csv', 'a', newline='') as file:
                     writer = csv.writer(file)
-                    extract_company_info(driver, writer)
-                time.sleep(3)
+                    get_company_info(driver, writer)
+
                 # Go back to the search results
                 driver.execute_script("window.history.go(-1)")
-                time.sleep(3)  # Wait for the page to load before continuing
 
-            except Exception as e:
-                print(f"Error processing result at index {i}: {e}")
-                continue
+            except:
+                print(f"Error processing result at index {i}")
+                # if 2+ error, go to next page
+                error_count += 1
+                if error_count > 2:
+                    break
 
         # Click on the next page
         try:
@@ -119,8 +128,7 @@ try:
             )
             next_page_link.click()
             page_number += 1
-            time.sleep(5)  # Adjust wait time as needed
-        except Exception as e:
+        except:
             print("No more pages or error navigating to the next page.")
             break
 
